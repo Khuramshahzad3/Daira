609ed8d152bea8e4399431ed7aadc76fb82bfa20
Khuram Shahzad <khuramshahzad54676@gmail.com>
2024-04-27 07:07:40 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container bg-slate-800">
+      <h3 className='text-green-500 font-semibold text-center'>TailwindCSS</h3>
+    </div>
+  )
+}
+
+export default App
